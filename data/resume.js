f5deea327ad6332c9ba03e25ed0a046045d9b3e2
Bradley Yeo <yeo.bradley@gmail.com>
2021-11-30 16:06:35 +0800

    Left with experience in resume page and projects

---
+++ data/resume.js
@@ -0,0 +1,16 @@
+const items = [
+  {
+    schoolName: 'Singapore Management University (Information Systems)',
+    major: 'Smart-City Management and IT Solution Management',
+    duration: 'Aug 2020 - May 2023',
+    coursework: 'Computational Thinking, Data Management, Analytics, CyberSecurity'
+  },
+  {
+    schoolName: 'Singapore Polytechnic (Business Administration)',
+    major: 'Operations Management',
+    duration: 'May 2016 - May 2018',
+    coursework: 'Market Intelligence, Operations Management, Quantitative Analysis'
+  }
+]
+
+export default items
--- pages/resume.js
+++ pages/resume.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import Main from '../layouts/Main'
+import items from '../data/resume'
 
 export async function getStaticProps() {
   const meta = {
@@ -18,6 +19,17 @@ export async function getStaticProps() {
 function Resume(props) {
   const { title, description, image } = props
 
+  const renderEducation = () => {
+    return items.map((item, index) => {
+      return <div style={{marginBottom: 40}} key={index}>
+        <h3>{item.schoolName}</h3>
+        <p>{item.major}</p>
+        <p>{item.duration}</p>
+        <p>{item.coursework}</p>
+        </div>
+    })
+  }
+
   return (
     <div className="single">
       <Head>
@@ -29,14 +41,24 @@ function Resume(props) {
         <meta content={`https://bradleyyeo.com${image}`} property="og:image" />
       </Head>
 
-      <p className="manifesto"><strong>Time is the most important asset.</strong> Time does not equal money. <strong>Time equals life.</strong> And you only have one chance to make it right. <strong>Every human being is fighting a battle inside themselves.</strong> It’s your <strong>obligation to help</strong> and inspire them. Regardless of what you do, you can always inspire others to do good. Nobody is better than you. <strong>And you’re not better than anybody else.</strong> Be humble. Being in the <strong>comfort zone is wonderful</strong>, but nothing ever grows there. Keep studying. <strong>Keep creating.</strong> Haters will come if you have the <strong>audacity to build something new</strong>. Don’t let them define you. <strong>Don’t let them stop you.</strong> Just block them and keep going. Don’t expect <strong>others to make you happy</strong>. You are the only one <strong>responsible for your happiness</strong>. Don’t wait until friday to enjoy life. Joy should be present in everything you do. <strong>Be kind to your parents</strong>. They gave up many things for you. Having hundreds of <strong>people admiring you</strong> is worthless, if you’re not admired by your own family. Don’t fear the unknown. Fear knowing everything. <strong>Life is too damn short</strong> and every day counts. <strong>Do what you wanna do</strong> and do it now.  Tick-tock don't stop. Tick-tock don't wait.</p>
-      <h2>Skills</h2>
-      <h3>Languages</h3>
-      <h3>Libraries, Frameworks, Tools</h3>
-      <h2>Education</h2>
-      <h2>Experience</h2>
-      <h2>Certification</h2>
+      <p className="manifesto">I am proficient in <strong>AWS, Python, Java, Javascript, HTML, CSS.</strong></p>
+      <section>
+        <h2>Skills</h2>
+        <h3>Languages</h3>
+        <p>Python (Pandas, NumPy, scikit-learn), SQL, HTML, CSS, Java, Javascript</p>
+        <h3>Libraries, Frameworks, Tools</h3>
+        <p>AWS, Git, Github, Bash, MongoDB, Vim, React, Node.js, Linux</p>
+      </section>
 
+      <h2>Education</h2>
+      {renderEducation()}
+      <h2>Experience</h2>
+
+      <h2>Certification</h2>
+      <p>AWS Certified IT Solution Architect Associate, AWS Certified Cloud Practitioner</p>
+      <a download className="btn-transparent btn-primary" role="button" href="/static/resume_pdf/Bradley_Yeo_Resume.pdf">
+        <i className="ri-download-2-line" /> Download Resume
+      </a>
     </div>
   )
 }

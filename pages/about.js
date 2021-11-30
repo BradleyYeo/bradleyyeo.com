2b500c745ea04c45a91fdd81164c4b47e45e26ab
Bradley Yeo <yeo.bradley@gmail.com>
2021-11-30 14:06:03 +0800

    add getDuration to about page

--- pages/about.js
+++ pages/about.js
@@ -78,7 +78,25 @@ function About(props) {
     })
   }
 
+  const getDuration = (startDate, endDate) => {
+    const durationObj = intervalToDuration({
+      start: parseISO(startDate),
+      end: endDate ? parseISO(endDate) : new Date()
+    })
 
+    let durationStr = ''
+
+    if (durationObj.years > 1) {
+      durationStr = `${durationObj.years} yrs `
+    }
+    else if (durationObj.years === 1) {
+      durationStr = `${durationObj.years} yr `
+    }
+
+    durationStr += `${durationObj.months} mos`
+
+    return durationStr
+  }
 
   const copyBio = (e) => {
     e.preventDefault()
